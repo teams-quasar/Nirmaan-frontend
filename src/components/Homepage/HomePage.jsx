64401f014b68d5469f845ae9e8c7680d56a8658c
Parth <parthmmyadav@gmail.com>
2024-07-24 17:45:26 +0530

    added home page

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -6,11 +6,20 @@ import AuthButtons from "./AuthButtons";
 import {Link,NavLink} from "react-router-dom";
 function HomePage() {
   return (
+    <>
     <header className="flex gap-5 justify-between items-start px-12 pt-5 pb-20 text-sm font-medium whitespace-nowrap bg-white max-md:flex-wrap max-md:px-5">
       <Logo />
       <Navigation />
       <AuthButtons />
     </header>
+    <div className="text-center">for other webpages use
+    <p>localhost/blogs</p>
+    <p>localhost/test</p>
+    <p>localhost/discover</p>
+    <p>localhost/wallet</p>
+    </div>
+    
+    </>
   );
 }
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -13,14 +13,16 @@ import SignUpPage from "./components/SignUp/SignUpPage/SignUpPage.jsx";
 import PrivateRoutes from "./utils/PrivateRoutes.jsx";
 import LoginPage from "./components/Login/LoginPage.jsx";
 import Wallet from "./components/Wallet/Wallet.jsx";
+import HomePage from "./components/Homepage/HomePage.jsx";
 // import ThemeSwitcher from "./features/theme/ThemeSwitcher";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route element={<PrivateRoutes />}>
+      <Route index element={<HomePage />} />
         <Route path="/" element={<Layout />}>
-          <Route index element={<MainContent />} />
+         
           <Route path="blogs" element={<BlogList />} />
           <Route path="wallet" element={<Wallet />} />
           <Route path="discover" element={<BlogList />} />
