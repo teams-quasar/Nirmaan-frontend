51bfe0656086a4e7703e9eb0b4a8e9680fa64882
Parth <parthmmyadav@gmail.com>
2024-08-24 14:10:35 +0530

    componenets

--- src/components/ExamSchemas/MainContent.jsx
+++ src/components/ExamSchemas/MainContent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ExamList from "./ExamList";
-import ExamSchemaTable from "./ExamSchemasTable/es-table"
+
+import ExamSchemaTable from "./ExamSchemasTable/es-table2"
 
 function MainContent() {
   return (
@@ -12,7 +12,26 @@ function MainContent() {
         A schema is blueprint from which the exam papers are created.
       </p>
       <div className="flex gap-5 justify-between mt-10 w-full max-md:flex-wrap max-md:max-w-full">
-        {/* <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full">
+        
+        <button className="flex gap-2 justify-center px-4 py-2 text-sm font-medium leading-6 text-white whitespace-nowrap bg-black rounded-md border border-gray-300 border-solid">
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/1c56d97cbb4bedc75230dcea7b2c5a69e019203c7d6ee158a02b323809a9bbc3?apiKey=8a82faa9db93454483a68c973b38c7b0&"
+            alt=""
+            className="shrink-0 my-auto w-4 aspect-square"
+          />
+          <span>New Exam Schema</span>
+        </button>
+      </div>
+      <ExamSchemaTable />
+    </div>
+  );
+}
+
+export default MainContent;
+
+
+{/* <div className="flex gap-5 max-md:flex-wrap max-md:max-w-full">
           <div className="flex flex-col grow shrink-0 justify-center text-base leading-6 text-gray-500 basis-0 w-fit">
             <div className="flex flex-col justify-center">
               <div className="flex flex-col justify-center">
@@ -43,19 +62,3 @@ function MainContent() {
             <span>View</span>
           </button>
         </div> */}
-        <button className="flex gap-2 justify-center px-4 py-2 text-sm font-medium leading-6 text-white whitespace-nowrap bg-black rounded-md border border-gray-300 border-solid">
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/1c56d97cbb4bedc75230dcea7b2c5a69e019203c7d6ee158a02b323809a9bbc3?apiKey=8a82faa9db93454483a68c973b38c7b0&"
-            alt=""
-            className="shrink-0 my-auto w-4 aspect-square"
-          />
-          <span>New Exam Schema</span>
-        </button>
-      </div>
-      <ExamSchemaTable />
-    </div>
-  );
-}
-
-export default MainContent;
